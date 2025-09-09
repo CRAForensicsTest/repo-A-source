48e951601b1cb932de89dab0107c1b19c30883b1
CRAForensicsTest <ssathishkumar@crai.com>
2025-09-09 13:31:47 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// This script runs when the page loads.
+console.log("Script.js is loaded successfully!");
+
+function showMessage() {
+    alert("This alert is from script.js!");
+}
+
+// You can call the function, for example, on a button click.
+// document.getElementById('myButton').addEventListener('click', showMessage);
